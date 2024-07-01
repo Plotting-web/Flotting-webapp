d47f40a91a3efcacf5bc76d1d167eb898cdefb3c
sunghun <sunghun5620@gmail.com>
2024-07-01 23:51:39 +0900

    [GYLEE-133] 조회 후 데이터 세팅 되도록 수정, 내 프로필 확인하기 화면에서 변경 요청 버튼 생성

--- src/components/profile/UserProfile.vue
+++ src/components/profile/UserProfile.vue
@@ -3,68 +3,60 @@ import CarouselLeft from "@/components/icon/CarouselLeft.vue";
 import ProfileImage from "@/components/image/ProfileImage.vue";
 import CarouselRight from "@/components/icon/CarouselRight.vue";
 import UserProfileNavigation from "@/components/profile/UserProfileNavigation.vue";
-import { characterOptions, getTitleByOptions, hobbyOptions, jobOptions, locationOptions, preferredDateOptions } from "@/views/signup/enum/options";
+import {
+    personalitiesOptions,
+    getTitleByOptions,
+    interestsOptions,
+    occupationTypeOptions,
+    residenceTypeOptions,
+    datePreferenceTypeOptions
+} from "@/views/signup/enum/options";
+import { onBeforeMount, ref } from "vue";
+import { instance } from "@/axios/axios";
+import router from "@/router";
+import { handleErrorAlerts } from "@/axios/common-api";
 
-defineProps({
+const props = defineProps({
+    userId: {
+        type: Number,
+        default: 0
+    },
     status: {
         type: String,
-        default: "default" // default, sending, pending, success, fail, self
-    },
-    profileImages: {
-        type: Array,
-        default: () => [
-            "https://image.fnnews.com/resource/media/image/2023/12/16/202312160703001203_l.jpg",
-            "https://i.namu.wiki/i/VE44Pr9Xrqrinf43GS7Yu945HyT3XyKWhmisPJcZ6qaCqV9J4oLnpUOHSrY-rjBh8j6ZI-3KtYkNf9x8sBe0Ew.webp",
-            "https://cdn.k-trendynews.com/news/photo/202312/162732_242532_2228.jpg"
-        ]
-    },
-    managerImage: {
-        type: String,
-        default: "https://image.kr.canon/pds/gallery/open/1257249062209_0d8F3Kb90P.jpg"
-    },
-    managerComment: {
-        type: String,
-        default:
-            "아담하면서도 볼륨있는 몸매 라인과 맑고 큰 눈이 돋보이는 여성 회원입니다. 또래에 비해 성숙한 사고와 뛰어난 공감 능력을 갖춘 다양한 매력을 가진 분입니다."
-    },
-    job: {
-        type: String,
-        default: "MINOR_COMPANY"
-    },
-    location: {
-        type: String,
-        default: "SEOUL_SOUTH"
-    },
-    height: {
-        type: Number,
-        default: 155
-    },
-    mbti: {
-        type: String,
-        default: "ENFP"
-    },
-    preferredDate: {
-        type: String,
-        default: "일주일에 1~2 번 데이트"
-    },
-    characters: {
-        type: Array,
-        default: () => ["EXTROVERTED", "CUTE", "HUMOROUS"]
-    },
-    hobbes: {
-        type: Array,
-        default: () => ["EXERCISE", "SELF_IMPROVEMENT", "READING"]
-    },
-    lifeStyle: {
-        type: String,
-        default:
-            "저는 병원에서 해외 국가들을 대상으로 마케팅을 하는 업무를 하고 있습니다. 성인이 되서 바로 경제적으로 독립해서 살고 있어서 또래들보다 철이 빨리 들었다는 이야기를 많이 들어요 ! 독립해서 살고 있어서 또래들보다 철이 빨리 들었다는 이야기를 많이 들어요 !"
-    },
-    somethingWantToSay: {
-        type: String,
-        default:
-            "저랑 잘 맞는 분을 만나게 된다면 퇴근 후 데이트나, 드라이브, 주말엔 종종 글램핑도 가고싶어요 :D 그리고 하고싶은 것도 많아서 이야기 하다보면 같이 가고 싶어지실꺼에요 ㅎ"
+        default: "default" // default, self
     }
+});
+
+const profile = ref({
+    profileImages: [""],
+    managerImage: "",
+    managerComment: "",
+    occupationType: "",
+    residenceType: "",
+    height: 0,
+    mbti: "",
+    datePreferenceType: "",
+    personalities: [],
+    interests: [],
+    lifestyle: "",
+    messageToFuturePartner: ""
+});
+
+onBeforeMount(() => {
+    const url = props.status === "self" ? `/users/v1/info` : `/profiles/v1/detail?userId=${props.userId}`;
+    instance.get(url).then(res => {
+        const statusCode = res?.status.statusCode;
+        if (statusCode !== "C000") {
+            handleErrorAlerts(statusCode);
+            switch (statusCode) {
+                case "U003":
+                    router.back();
+                    break;
+            }
+            return;
+        }
+        // TODO : API 정리되면 데이터 세팅 추가해야함.
+    });
 });
 </script>
 
@@ -77,34 +69,34 @@ defineProps({
             <template #next="{ props }">
                 <carousel-right @click="props.onClick"></carousel-right>
             </template>
-            <v-carousel-item v-for="(src, idx) in profileImages" :key="`profile-item-${idx}`">
+            <v-carousel-item v-for="(src, idx) in profile.profileImages" :key="`profile-item-${idx}`">
                 <profile-image :src="src" />
             </v-carousel-item>
         </v-carousel>
         <div class="prf-manager-comment-layout">
             <div class="prf-manager-title-layout">
-                <div class="prf-manager-title-img" :style="`background-image: url(${managerImage})`"></div>
+                <div class="prf-manager-title-img" :style="`background-image: url(${profile.managerImage})`"></div>
                 <span class="prf-manager-title-text">
                     매니저의 코멘트를 살펴보세요 !
                 </span>
             </div>
             <div class="prf-manager-comment-text">
-                {{ managerComment }}
+                {{ profile.managerComment }}
             </div>
         </div>
         <div class="prf-main-layout">
             <div class="prf-main-1-layout">
                 <div class="prf-main-1-icon-layout">
                     <v-icon icon="mdi-account-circle" size="30" />
-                    <span class="prf-main-1-text">{{ getTitleByOptions(jobOptions, job) }}</span>
+                    <span class="prf-main-1-text">{{ getTitleByOptions(occupationTypeOptions, profile.occupationType) }}</span>
                 </div>
                 <div class="prf-main-1-icon-layout">
                     <v-icon icon="mdi-map-marker-radius" size="30" />
-                    <span class="prf-main-1-text">{{ getTitleByOptions(locationOptions, location) }}</span>
+                    <span class="prf-main-1-text">{{ getTitleByOptions(residenceTypeOptions, profile.residenceType) }}</span>
                 </div>
                 <div class="prf-main-1-icon-layout">
                     <v-icon icon="mdi-format-list-bulleted" size="30" />
-                    <span class="prf-main-1-text">{{ height }}CM</span>
+                    <span class="prf-main-1-text">{{ !!profile.height ? `${profile.height}CM` : "" }}</span>
                 </div>
             </div>
             <div class="prf-main-2-layout">
@@ -113,21 +105,21 @@ defineProps({
                 </div>
                 <div class="prf-main-2-content">
                     <div class="prf-main-2-row">
-                        <div class="prf-main-2-item" style="width: auto;">
-                            {{ mbti }}
+                        <div v-show="!!profile.mbti" class="prf-main-2-item" style="width: auto;">
+                            {{ profile.mbti }}
                         </div>
-                        <div class="prf-main-2-item">
-                            {{ preferredDate }}
-                        </div>
-                    </div>
-                    <div class="prf-main-2-row">
-                        <div v-for="(val, idx) in characters" :key="`profile-character-${idx}`" class="prf-main-2-item">
-                            {{ getTitleByOptions(characterOptions, val) }}
+                        <div v-show="!!profile.datePreferenceType" class="prf-main-2-item">
+                            {{ getTitleByOptions(datePreferenceTypeOptions, profile.datePreferenceType) }}
                         </div>
                     </div>
                     <div class="prf-main-2-row">
-                        <div v-for="(val, idx) in hobbes" :key="`profile-hobby-${idx}`" class="prf-main-2-item">
-                            {{ getTitleByOptions(hobbyOptions, val) }}
+                        <div v-for="(val, idx) in profile.personalities" :key="`profile-character-${idx}`" class="prf-main-2-item">
+                            {{ getTitleByOptions(personalitiesOptions, val) }}
+                        </div>
+                    </div>
+                    <div class="prf-main-2-row">
+                        <div v-for="(val, idx) in profile.interests" :key="`profile-hobby-${idx}`" class="prf-main-2-item">
+                            {{ getTitleByOptions(interestsOptions, val) }}
                         </div>
                     </div>
                 </div>
@@ -138,19 +130,19 @@ defineProps({
                 </div>
                 <div class="prf-main-3-divider" />
                 <div class="prf-main-3-text">
-                    {{ lifeStyle }}
+                    {{ profile.lifestyle }}
                 </div>
                 <div class="prf-main-4-title">
                     나의 미래 연인에게 하고싶은 말
                 </div>
                 <div class="prf-main-4-divider" />
                 <div class="prf-main-4-text">
-                    {{ somethingWantToSay }}
+                    {{ profile.messageToFuturePartner }}
                 </div>
             </div>
         </div>
         <div style="display: flex; flex-direction: column; gap: 10px;">
-            <user-profile-navigation :status="status" />
+            <user-profile-navigation :status="status" :user-id="userId" />
         </div>
     </div>
 </template>
--- src/views/setting/SettingMyProfile.vue
+++ src/views/setting/SettingMyProfile.vue
@@ -2,12 +2,13 @@
 import MainHeader from "@/components/layout/MainHeader.vue";
 import MainBody from "@/components/layout/MainBody.vue";
 import UserProfile from "@/components/profile/UserProfile.vue";
+import { userStore } from "@/store/userStore";
 </script>
 
 <template>
     <main-header start="back" end="store" title />
     <main-body is-header>
-        <user-profile status="self" />
+        <user-profile status="self" :user-id="userStore().getId()" />
     </main-body>
 </template>
 
